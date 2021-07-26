c590cbeffd34d000e54775f42505e8a07f826428
Malcak <malcakx@gmail.com>
2021-07-26 16:51:20 -0500

    chore: add helmet package

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import path from 'path';
 
 import { loadApiEndpoints } from './controllers/api';
@@ -10,6 +11,7 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.use(
   express.static(path.join(__dirname, '../public'), { maxAge: 31557600000 })
