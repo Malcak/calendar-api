1d4241142a4b4ef7eb3a2c5423a54753b8879197
Malcak <malcakx@gmail.com>
2021-11-10 16:27:44 -0500

    refactor(jwt): validate the existence of jst secret key

--- src/shared/infrastructure/helpers/jwt.ts
+++ src/shared/infrastructure/helpers/jwt.ts
@@ -6,21 +6,19 @@ const genJWT = (
   email: string
 ): Promise<string | undefined> => {
   return new Promise((resolve, reject) => {
+    const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY;
     const payload = { _id, name, email };
-    jwt.sign(
-      payload,
-      process.env.SECRET_JWT_KEY || '',
-      {
-        expiresIn: '2h',
-      },
-      (err, token) => {
+    if (SECRET_JWT_KEY) {
+      jwt.sign(payload, SECRET_JWT_KEY, { expiresIn: '2h' }, (err, token) => {
         if (err) {
-          console.log(err);
+          console.error(err);
           reject('token could not be generated');
         }
         resolve(token);
-      }
-    );
+      });
+    } else {
+      throw new Error('jwt secret key is required to sign the json web tokens');
+    }
   });
 };
 
